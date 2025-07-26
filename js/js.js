91202b9f7fd74a7968ea989ea67eabc33ca73b35
Fyair09 <fyair09@gmail.com>
2025-07-25 20:38:25 -0600

    pdf embebido V11

--- js/js.js
+++ js/js.js
@@ -1,3 +1,15 @@
   // Bloquea el clic derecho
     document.addEventListener('contextmenu', function(e) {
       e.preventDefault();
+
+      // Bloquea menú contextual en móviles (mantener pulsado)
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    });
+
+    // Bloquea pulsación larga
+    document.addEventListener('touchstart', function preventTouch(e) {
+      if (e.target.tagName === 'IMG') {
+        e.preventDefault();
+      }
+    }, { passive: false });
